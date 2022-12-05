6a3d49f0e1bb39f11b5030bd9efb122daf3661ea
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-12-05 13:01:56 -0500

    initial commit (fixed)

---
+++ HellowWorldFSharp/Program.fs
@@ -0,0 +1,5 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let name = "Mario"
+let time = System.DateTime.Now
+printfn $"Hello from F#! My name is {name}, the time is {time}"
