b0c0fa18458ef2b46de48f008c2d53cbfd60f85f
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-12-05 17:16:03 -0500

    create a function

--- HellowWorldFSharp/Program.fs
+++ HellowWorldFSharp/Program.fs
@@ -1,5 +1,10 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
 let name = "Mario"
 let time = System.DateTime.Now
 printfn $"Hello from F#! My name is {name}, the time is {time}"
+
+let greetPerson firstName age = $"Hello {firstName}, you are {age} years old"
+let text = greetPerson "Mario" 56
+printfn $"{text}"
