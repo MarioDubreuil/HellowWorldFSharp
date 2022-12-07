785949a72d527f4692821c242f26e1240ce64e4a
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-12-07 08:10:09 -0500

    exercise 3.1

--- HellowWorldFSharp/Program.fs
+++ HellowWorldFSharp/Program.fs
@@ -8,3 +8,11 @@ printfn $"Hello from F#! My name is {name}, the time is {time}"
 let greetPerson firstName age = $"Hello {firstName}, you are {age} years old"
 let text = greetPerson "Mario" 56
 printfn $"{text}"
+
+let myFunction x y z =
+    let inProgress = x + y
+    let answer = inProgress * z
+    $"The answer is {answer}"
+
+let myAnswer = myFunction 10 20 30
+printfn $"{myAnswer}"
